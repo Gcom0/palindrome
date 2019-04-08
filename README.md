28fa124cce1e12d036ae2458226a9456c52601d8
Giancarlo Comelli <giancarlo.comelli@gmail.com>
2019-04-08 19:39:04 +0000

    Finish working and refactored palindrome method

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# palindrome
+# Phrase object (with palindrome detector)
+
+This is a sample NPM module created in Learn Enough JavaScript to Be Dangerous by Michael Hartl.
+
+The module can be used as follows:
+
+```
+$ npm install --global mhartl-palindrome
+$ vim test.js
+let Phrase = require("mhartl-palindrome");
+let napoleonsLament = new Phrase("Able was I, ere I saw Elba.");
+console.log(napoleonsLament.palindrome());
+$ node test.js
+true
+```
--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 "use strict";
 
+module.exports = Phrase;
+
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 };
 
-/*String.prototype.blank = function() {
-  return this.includes('' || /^\s+$/);
+String.prototype.blank = function() {
+  if (this.match(/^\s+$/)) {
+    return true;
+  }
 };
-*/
+
 Array.prototype.last = function() {
   return this.slice(-1);
 };
+
 
 // Defines a Phrase object.
 function Phrase(content) {
@@ -19,7 +24,14 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
+  };
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
   };
 
   // Returns true if the phrase is a palindrome, false otherwise.
